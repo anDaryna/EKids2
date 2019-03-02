f1e4895d9d55f1bdb67ff5da94b2663ee6b3001f
Children <Children@EPUAKYIW1132.kyiv.epam.com>
2019-03-02 13:18:38 +0200

    Initial commit1

---
+++ venv/DarynaP1.py
@@ -0,0 +1,16 @@
+import random
+name = ["Anna" , "Tom" , "Angela" , "Max"]
+do = ["cook" , "eat" , "sleep" , "do homework"]
+random.shuffle (name)
+random.shuffle (do)
+for item in range (1):
+    print (name[0] + " " + do[1] + ".")
+
+for item in range (1):
+    print (name[1] + " " + do[2] + ".")
+
+for item in range (1):
+    print (name[2] + " " + do[3] + ".")
+
+for item in range (1):
+    print (name[3] + " " + do[0] + ".")
